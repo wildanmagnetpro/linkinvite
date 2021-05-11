32c11fd5e9341fb0f8348d691da9c58a5ca4e43c
wildanmagnetpro <60187646+wildanmagnetpro@users.noreply.github.com>
2021-05-11 12:44:18 +0700

    TELAGA2760
    TELAGA2760

---
+++ TELAGA/2760/skin.js
@@ -0,0 +1,224 @@
+// Garden Gnome Software - Skin
+// Pano2VR 6.0.3/17298
+// Filename: telaga2260.ggsk
+// Generated 2021-05-11T12:40:35
+
+function pano2vrSkin(player,base) {
+	var me=this;
+	var skin=this;
+	var flag=false;
+	var skinKeyPressed = 0;
+	this.player=player;
+	this.player.skinObj=this;
+	this.divSkin=player.divSkin;
+	this.ggUserdata=player.userdata;
+	this.lastSize={ w: -1,h: -1 };
+	var basePath="";
+	// auto detect base path
+	if (base=='?') {
+		var scripts = document.getElementsByTagName('script');
+		for(var i=0;i<scripts.length;i++) {
+			var src=scripts[i].src;
+			if (src.indexOf('skin.js')>=0) {
+				var p=src.lastIndexOf('/');
+				if (p>=0) {
+					basePath=src.substr(0,p+1);
+				}
+			}
+		}
+	} else
+	if (base) {
+		basePath=base;
+	}
+	this.elementMouseDown=[];
+	this.elementMouseOver=[];
+	var cssPrefix='';
+	var domTransition='transition';
+	var domTransform='transform';
+	var prefixes='Webkit,Moz,O,ms,Ms'.split(',');
+	var i;
+	var hs,el,els,elo,ela,elHorScrollFg,elHorScrollBg,elVertScrollFg,elVertScrollBg,elCornerBg;
+	if (typeof document.body.style['transform'] == 'undefined') {
+		for(var i=0;i<prefixes.length;i++) {
+			if (typeof document.body.style[prefixes[i] + 'Transform'] !== 'undefined') {
+				cssPrefix='-' + prefixes[i].toLowerCase() + '-';
+				domTransition=prefixes[i] + 'Transition';
+				domTransform=prefixes[i] + 'Transform';
+			}
+		}
+	}
+	
+	player.setMargins(0,0,0,0);
+	
+	this.updateSize=function(startElement) {
+		var stack=[];
+		stack.push(startElement);
+		while(stack.length>0) {
+			var e=stack.pop();
+			if (e.ggUpdatePosition) {
+				e.ggUpdatePosition();
+			}
+			if (e.hasChildNodes()) {
+				for(var i=0;i<e.childNodes.length;i++) {
+					stack.push(e.childNodes[i]);
+				}
+			}
+		}
+	}
+	
+	this.callNodeChange=function(startElement) {
+		var stack=[];
+		stack.push(startElement);
+		while(stack.length>0) {
+			var e=stack.pop();
+			if (e.ggNodeChange) {
+				e.ggNodeChange();
+			}
+			if (e.hasChildNodes()) {
+				for(var i=0;i<e.childNodes.length;i++) {
+					stack.push(e.childNodes[i]);
+				}
+			}
+		}
+	}
+	player.addListener('configloaded', function() { me.callNodeChange(me.divSkin); });
+	player.addListener('changenode', function() { me.ggUserdata=player.userdata; me.callNodeChange(me.divSkin); });
+	
+	var parameterToTransform=function(p) {
+		var hs='translate(' + p.rx + 'px,' + p.ry + 'px) rotate(' + p.a + 'deg) scale(' + p.sx + ',' + p.sy + ')';
+		return hs;
+	}
+	
+	this.findElements=function(id,regex) {
+		var r=[];
+		var stack=[];
+		var pat=new RegExp(id,'');
+		stack.push(me.divSkin);
+		while(stack.length>0) {
+			var e=stack.pop();
+			if (regex) {
+				if (pat.test(e.ggId)) r.push(e);
+			} else {
+				if (e.ggId==id) r.push(e);
+			}
+			if (e.hasChildNodes()) {
+				for(var i=0;i<e.childNodes.length;i++) {
+					stack.push(e.childNodes[i]);
+				}
+			}
+		}
+		return r;
+	}
+	
+	this.addSkin=function() {
+		var hs='';
+		this.ggCurrentTime=new Date().getTime();
+		el=me._text_1=document.createElement('div');
+		els=me._text_1__text=document.createElement('div');
+		el.className='ggskin ggskin_textdiv';
+		el.ggTextDiv=els;
+		el.ggId="Text 1";
+		el.ggParameter={ rx:0,ry:0,a:0,sx:1,sy:1 };
+		el.ggVisible=true;
+		el.className="ggskin ggskin_text ";
+		el.ggType='text';
+		hs ='';
+		hs+='bottom : 62px;';
+		hs+='height : 93px;';
+		hs+='position : absolute;';
+		hs+='right : 13px;';
+		hs+='visibility : inherit;';
+		hs+='width : 405px;';
+		hs+='pointer-events:auto;';
+		el.setAttribute('style',hs);
+		el.style[domTransform + 'Origin']='50% 50%';
+		hs ='position:absolute;';
+		hs += 'box-sizing: border-box;';
+		hs+='cursor: default;';
+		hs+='right: 0px;';
+		hs+='bottom:  0px;';
+		hs+='width: 405px;';
+		hs+='height: 93px;';
+		hs+='border: 0px solid #ffffff;';
+		hs+='border-radius: 5px;';
+		hs+=cssPrefix + 'border-radius: 5px;';
+		hs+='color: rgba(255,255,206,1);';
+		hs+='font-size: 16px;';
+		hs+='font-weight: inherit;';
+		hs+='text-align: right;';
+		hs+='white-space: nowrap;';
+		hs+='padding: 11px 22px 11px 22px;';
+		hs+='overflow: hidden;';
+		els.setAttribute('style',hs);
+		els.innerHTML="TELAGA LEGOK<br\/>Harga Rp 312 Juta<br\/>Cicilan KPR Mulia 2.5 juta\/bulan<br\/>Hubungi Wildan - 083894553550";
+		el.appendChild(els);
+		me._text_1.ggIsActive=function() {
+			return false;
+		}
+		el.ggElementNodeId=function() {
+			return player.getCurrentNode();
+		}
+		me._text_1.ggUpdatePosition=function (useTransition) {
+		}
+		me.divSkin.appendChild(me._text_1);
+		el=me._image_1=document.createElement('div');
+		els=me._image_1__img=document.createElement('img');
+		els.className='ggskin ggskin_image_1';
+		hs=basePath + 'images/image_1.png';
+		els.setAttribute('src',hs);
+		els.ggNormalSrc=hs;
+		els.setAttribute('style','position: absolute;top: 0px;left: 0px;width: 100%;height: 100%;-webkit-user-drag:none;pointer-events:none;;');
+		els.className='ggskin ggskin_image';
+		els['ondragstart']=function() { return false; };
+		player.checkLoaded.push(els);
+		el.appendChild(els);
+		el.ggSubElement = els;
+		el.ggId="Image 1";
+		el.ggParameter={ rx:0,ry:0,a:0,sx:1,sy:1 };
+		el.ggVisible=true;
+		el.className="ggskin ggskin_image ";
+		el.ggType='image';
+		hs ='';
+		hs+='bottom : 6px;';
+		hs+='height : 55px;';
+		hs+='position : absolute;';
+		hs+='right : 32px;';
+		hs+='visibility : inherit;';
+		hs+='width : 138px;';
+		hs+='pointer-events:auto;';
+		el.setAttribute('style',hs);
+		el.style[domTransform + 'Origin']='50% 50%';
+		me._image_1.ggIsActive=function() {
+			return false;
+		}
+		el.ggElementNodeId=function() {
+			return player.getCurrentNode();
+		}
+		me._image_1.onclick=function (e) {
+			player.openUrl("https:\/\/wa.me\/6283894553550","_self");
+		}
+		me._image_1.ggUpdatePosition=function (useTransition) {
+		}
+		me.divSkin.appendChild(me._image_1);
+		player.addListener('sizechanged', function() {
+			me.updateSize(me.divSkin);
+		});
+	};
+	this.hotspotProxyClick=function(id, url) {
+	}
+	this.hotspotProxyDoubleClick=function(id, url) {
+	}
+	me.hotspotProxyOver=function(id, url) {
+	}
+	me.hotspotProxyOut=function(id, url) {
+	}
+	player.addListener('changenode', function() {
+		me.ggUserdata=player.userdata;
+	});
+	me.skinTimerEvent=function() {
+		me.ggCurrentTime=new Date().getTime();
+	};
+	player.addListener('timer', me.skinTimerEvent);
+	me.addSkin();
+	me.skinTimerEvent();
+};
